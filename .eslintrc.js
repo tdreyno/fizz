ab4044075cdc366cc427ed14e0272a5502392f40
Thomas Reynolds <me@tdreyno.com>
2020-02-18 10:38:31 -0800

    ci(release): enable CI publishing
    Turn on semantic-release on Github Actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
   env: {
+    node: true,
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: [
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/require-await": 0
-  }
+    "@typescript-eslint/require-await": 0,
+  },
 };
