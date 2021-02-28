c43dc664ac2f86d9c726a2556706a5563371f076
Thomas Reynolds <me@tdreyno.com>
2021-02-28 07:47:43 +0000

    DERP

--- src/react/__tests__/loading/machine/states/Initializing.ts
+++ src/react/__tests__/loading/machine/states/Initializing.ts
@@ -4,15 +4,11 @@ import { StartLoading, startLoading } from "../actions"
 import { Shared } from "../types"
 import Loading from "./Loading"
 
-type ValidActions = Enter | StartLoading
+type Actions = Enter | StartLoading
 type Data = [Shared, boolean]
 
-export default state<ValidActions, Data>(
-  {
-    Enter: () => startLoading(),
+export default state<Actions, Data>({
+  Enter: startLoading,
 
-    StartLoading: ([shared]) => Loading([shared, "test"]),
-  },
-
-  { debugName: "Initializing" },
-)
+  StartLoading: ([shared]) => Loading([shared, "test"]),
+})
--- src/react/__tests__/loading/machine/states/Loading.ts
+++ src/react/__tests__/loading/machine/states/Loading.ts
@@ -5,21 +5,18 @@ import { loadData } from "../effects"
 import { Shared } from "../types"
 import Ready from "./Ready"
 
-type ValidActions = Enter | FinishedLoading | Update
+type Actions = Enter | FinishedLoading | Update
 type Data = [Shared, string]
 
-export default state<ValidActions, Data>(
-  {
-    Enter: () => loadData(),
+export default state<Actions, Data>({
+  Enter: loadData,
 
-    FinishedLoading: ([shared], name) =>
-      Ready({ ...shared, message: `Hi, ${name}` }),
+  FinishedLoading: ([shared], name) =>
+    Ready({ ...shared, message: `Hi, ${name}` }),
 
-    Update: ([shared, str], _, { update }) => {
-      shared.message = shared.message + " " + shared.message
-      return update([shared, str])
-    },
-  },
-
-  { debugName: "Loading" },
-)
+  Update: ([shared, str], _, { update }) =>
+    update([
+      { ...shared, message: shared.message + " " + shared.message },
+      str,
+    ]),
+})
